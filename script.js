b3c33bc0c8dff8559479f1920157fdb5627719d8
samueldhoore3 <119854539+samueldhoore3@users.noreply.github.com>
2022-12-06 02:52:40 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
+// loading
 var el = document.getElementById('loading'),
     i = 0,
     load = setInterval(function() {
       i = ++i % 4;
       el.innerHTML = 'Welcome' + Array(i + 1).join('.');
 }, 600);
+
+// slide overlay
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+  }
+  
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
