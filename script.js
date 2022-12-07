fb3d2a1600a0c9f48469d2293087e506c4c77283
samueldhoore3 <119854539+samueldhoore3@users.noreply.github.com>
2022-12-07 19:01:25 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,31 @@
 // loading
-var el = document.getElementById('loading'),
-    i = 0,
-    load = setInterval(function() {
-      i = ++i % 4;
-      el.innerHTML = 'Welcome' + Array(i + 1).join('.');
-}, 600);
+  var el = document.getElementById('loading'),
+  i = 0,
+  load = setInterval(function() {
+    i = ++i % 4;
+    el.innerHTML = "/" + Array(i + 1).join('.');
+  }, 600);
 
 // slide overlay
-function openNav() {
+  function openNav() {
     document.getElementById("myNav").style.width = "100%";
   }
-  
+
   function closeNav() {
     document.getElementById("myNav").style.width = "0%";
   }
+
+//play pause audio
+  var myAudio = document.getElementById("myAudio");
+  var isPlaying = false;
+
+  function togglePlay() {
+  isPlaying ? myAudio.pause() : myAudio.play();
+  };
+
+  myAudio.onplaying = function() {
+  isPlaying = true;
+  };
+  myAudio.onpause = function() {
+  isPlaying = false;
+  };
